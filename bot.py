d58190a3db3384a35914975fed56c3d246bdb11a
luceatnobis <wehrmeyer.martin@web.de>
2016-01-08 20:00:14 +0100

    First attempt at fixing imgur (incomplete!)

--- bot.py
+++ bot.py
@@ -8,92 +8,16 @@ import json
 import hashlib
 
 import irc3
-"""
-<<<<<<< HEAD
-from harvester import gyazo
-from harvester import imgur
-from harvester import ppomf
-from harvester import bpaste
-from harvester import dpaste
-from harvester import infotomb
-from harvester import prntscrn
-from harvester import hastebin
-from harvester import pastebin
-from harvester import cubeupload
-
-from harvester import libDataBs
-
-
-@irc3.plugin
-class brotherBot:
-
-    requires = [
-        'irc3.plugins.core',
-        'irc3.plugins.userlist',
-        'irc3.plugins.command',
-        'irc3.plugins.human',
-    ]
-    harvested_channels = [
-        '#brotherBot',
-    ]
-
-    def __init__(self, bot):
-        self.bot = bot
-
-    #NOTE: https://irc3.readthedocs.org/en/latest/rfc.html
-    @irc3.event(irc3.rfc.PRIVMSG)
-    def privmsg_trigger(self, mask=None, event=None, target=None, data=None):
-        if not all([mask, event, target, data]):
-            raise Exception("shits fucked up yo")
-        if target not in self.harvested_channels:
-            return
-
-        nick, user, host = self.split_mask(mask)
-        url = self.urlReg(data)
-        if url:
-            harvest(mask, url, self.bot, target)
-
-    def split_mask(self, mask_raw):
-        nick, _ = mask_raw.split('!')
-        user, host = _.split('@')
-        return (nick, user, host)
-
-
-def main():
-
-    bot = irc3.IrcBot(
-        nick='joyTheRipper', autojoins=['#brotherBot'],
-        host='irc.freenode.net', port=6697, ssl=True,
-        includes=[
-            'irc3.plugins.core',
-            'irc3.plugins.command',
-
-            'irc3.plugins.human',
-            __name__,
-        ]
-    )
-=======
-"""
 from harvester.settings import BotSettings
 #  from harvester.utils import urlReg, harvest
 
 if __name__ == "__main__":
+    import pdb
+    pdb.set_trace()
     bot = irc3.IrcBot(**(BotSettings.getSettings()))
     bot.include('irc3.plugins.log')
     bot.include('harvester.harvester')
     bot.run()
-"""
->>>>>>> 4f5b0caa29f92547c36b4ce23a1ba6af06d8484e
-    bot.include('irc3.plugins.log')
-    bot.include('harvester.harvester')
-    bot.run()
-
-<<<<<<< HEAD
-main()
-
-=======
- >>>>>>> 4f5b0caa29f92547c36b4ce23a1ba6af06d8484e
-"""
 
 urls = [
     'http://gyazo.com/fc12a9bb2a4b92d1debef49b8279371f',
--- harvester/imgur/__init__.py
+++ harvester/imgur/__init__.py
@@ -1,17 +1,58 @@
-#!/usr/bin/env python
-import requests
+#!/usr/bin/env python3
 import re
+import pdb
+import requests
+
+from urllib.parse import urlparse
+
+# import imgurpython
 from bs4 import BeautifulSoup
+
+from harvester import auth
+
+
 def get_content(url):
+
     paste_info = {
         'site': 'imgur',
         'url': url
     }
-    m = re.match('^.*com(?:/gallery)?/([0-9a-zA-Z]+)(?:\.([a-zA-Z]+))?$',url)
+
+    # methods that process urls like imgur.com/gallery/ and imgur.com/a/
+    chain_links = {
+        'a': _return_a,
+        'gallery': _return_gallery,
+    }
+    # original regex (doesnt match /a/)
+    # m = re.match('^.*com(?:/gallery)?/([0-9a-zA-Z]+)(?:\.([a-zA-Z]+))?$', url)
+    # modified regex that fell out of favour (brotherBox))
+    # m = re.match('^.*com(?:/((gallery|a)/([0-9a-zA-Z]+)|(?:\.([a-zA-Z]+))?$))', url)
+
+    parsed = urlparse(url)
+    if parsed.netloc != "imgur.com":
+        pass  # TODO: think of something smart
+
+    """
+    imgurclient = imgurpython.ImgurClient(
+        auth.imgur_client_id, auth.imgur_client_secrets)
+    """
+
+    path = parsed.path.split("/")[1:]
+    n_elements = len(path)
+
+    if n_elements == 1:  # we probably have a single piece of content
+        pass
+    elif n_elements == 2:  # we have either /a/ or /gallery/
+        fun = chain_links.get(path[0])
+        if fun is None:
+            raise NotImplementedError(path[0])
+        # foo = fun(imgurclient, path[1])
+        foo = fun(path[1])
+    else:
+        pass  # TODO: think of something for imgur.com/foo/bar/baz[/bam...]
+    """
     response = requests.get(url)
     if response.status_code != 200:
-        return
-    if not m.group(2):
         soup = BeautifulSoup(response.text)
         url1 =  soup.find('meta', {'property': 'og:image'})['content']
         m = re.match('^.*com/([0-9a-zA-Z]+)\.([a-zA-Z]+)$',url1)
@@ -23,7 +64,23 @@ def get_content(url):
     paste_info['orig_filename'] = m.group(1)
     paste_info['content'] = response.content
     return paste_info
+    """
+
 '''
 url = 'http://imgur.com/gallery/aChgMdG'
 print get_content(url)
 '''
+
+def _return_single_content(foo):
+    pass
+
+def _return_a(foo):
+    pass
+
+def _return_gallery(client, gallery_id):
+    query = client.gallery_search(gallery_id)
+    gallery = [x for x in query if x.id == gallery_id]
+    if not gallery:  # we have an invalid gallery i believe
+        pass
+    gallery = gallery[0]
+    pdb.set_trace()
